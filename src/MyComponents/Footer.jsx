47f6dd979a7bc029f55537e7a675f34574e92314
ShivasunuGupta <shivasunugupta@gmail.com>
2025-08-15 14:40:45 +0530

    Update Footer.jsx

--- src/MyComponents/Footer.jsx
+++ src/MyComponents/Footer.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <div className='py-8 pb-24 mt-8 border-t border-gray-800 text-center'>
-            <p className='flex items-center justify-center '>Made with <Heart className="h-4 w-4 mx-2 text-red-500" /> by Shivasunu Gupta</p>
+            <p className='flex items-center justify-center '>404: 😴 not found </p>
         </div>
     )
 }
